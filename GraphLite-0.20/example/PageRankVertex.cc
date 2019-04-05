b53821957792e940387258ded5cad582eacce09d
742362144 <742362144@qq.com>
2019-04-05 13:59:10 +0800

    hw2-2

--- GraphLite-0.20/example/PageRankVertex.cc
+++ GraphLite-0.20/example/PageRankVertex.cc
@@ -3,11 +3,11 @@
  * @author  Songjie Niu, Shimin Chen
  * @version 0.1
  *
- * @section LICENSE 
- * 
+ * @section LICENSE
+ *
  * Copyright 2016 Shimin Chen (chensm@ict.ac.cn) and
  * Songjie Niu (niusongjie@ict.ac.cn)
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -19,9 +19,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * @section DESCRIPTION
- * 
+ *
  * This file implements the PageRank algorithm using graphlite API.
  *
  */
@@ -29,12 +29,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
-#include <set>
-#include <map>
 
 #include "GraphLite.h"
 
 #define VERTEX_CLASS_NAME(name) PageRankVertex##name
+
+#define EPS 1e-6
 
 #define IN_NEIGHBOR 1
 #define OUT_NEIGHBOR 2
@@ -51,119 +51,114 @@ struct Counter {
 	int64_t cycle = 0;
 };
 
-// 处理输入
-class VERTEX_CLASS_NAME(InputFormatter): public InputFormatter {
+class VERTEX_CLASS_NAME(InputFormatter) : public InputFormatter {
 public:
-    int64_t getVertexNum() {
-        unsigned long long n;
-        sscanf(m_ptotal_vertex_line, "%lld", &n);
-        m_total_vertex= n;
-        return m_total_vertex;
-    }
-    int64_t getEdgeNum() {
-        unsigned long long n;
-        sscanf(m_ptotal_edge_line, "%lld", &n);
-        m_total_edge= n;
-        return m_total_edge;
-    }
-    int getVertexValueSize() {
-        m_n_value_size = sizeof(double);
-        return m_n_value_size;
-    }
-    int getEdgeValueSize() {
-        m_e_value_size = sizeof(double);
-        return m_e_value_size;
-    }
-    int getMessageValueSize() {
-        m_m_value_size = sizeof(double);
-        return m_m_value_size;
-    }
-    void loadGraph() {
-        if (m_total_edge <= 0)  return;
+	int64_t getVertexNum() {
+		unsigned long long n;
+		sscanf(m_ptotal_vertex_line, "%lld", &n);
+		m_total_vertex = n;
+		return m_total_vertex;
+	}
+	int64_t getEdgeNum() {
+		unsigned long long n;
+		sscanf(m_ptotal_edge_line, "%lld", &n);
+		m_total_edge = n;
+		return m_total_edge;
+	}
+	int getVertexValueSize() {
+		m_n_value_size = sizeof(double);
+		return m_n_value_size;
+	}
+	int getEdgeValueSize() {
+		m_e_value_size = sizeof(double);
+		return m_e_value_size;
+	}
+	int getMessageValueSize() {
+		m_m_value_size = sizeof(double);
+		return m_m_value_size;
+	}
+	void loadGraph() {
+		if (m_total_edge <= 0)  return;
 
-        unsigned long long last_vertex;
-        unsigned long long from;
-        unsigned long long to;
-        double weight = 0;
-        
-        double value = 1;
-        int outdegree = 0;
-        
-        const char *line= getEdgeLine();
+		unsigned long long last_vertex;
+		unsigned long long from;
+		unsigned long long to;
+		double weight = 0;
 
-        // Note: modify this if an edge weight is to be read
-        //       modify the 'weight' variable
+		double value = 1;
+		int outdegree = 0;
 
-        sscanf(line, "%lld %lld", &from, &to);
-        addEdge(from, to, &weight);
+		const char *line = getEdgeLine();
 
-        last_vertex = from;
-        ++outdegree;
-        for (int64_t i = 1; i < m_total_edge; ++i) {
-            line= getEdgeLine();
+		// Note: modify this if an edge weight is to be read
+		//       modify the 'weight' variable
 
-            // Note: modify this if an edge weight is to be read
-            //       modify the 'weight' variable
+		sscanf(line, "%lld %lld", &from, &to);
+		addEdge(from, to, &weight);
 
-            sscanf(line, "%lld %lld", &from, &to);
-            if (last_vertex != from) {
-                addVertex(last_vertex, &value, outdegree);
-                last_vertex = from;
-                outdegree = 1;
-            } else {
-                ++outdegree;
-            }
-            addEdge(from, to, &weight);
-        }
-        addVertex(last_vertex, &value, outdegree);
-    }
+		last_vertex = from;
+		++outdegree;
+		for (int64_t i = 1; i < m_total_edge; ++i) {
+			line = getEdgeLine();
+
+			// Note: modify this if an edge weight is to be read
+			//       modify the 'weight' variable
+
+			sscanf(line, "%lld %lld", &from, &to);
+			if (last_vertex != from) {
+				addVertex(last_vertex, &value, outdegree);
+				last_vertex = from;
+				outdegree = 1;
+			}
+			else {
+				++outdegree;
+			}
+			addEdge(from, to, &weight);
+		}
+		addVertex(last_vertex, &value, outdegree);
+	}
 };
 
-// 负责输出
-class VERTEX_CLASS_NAME(OutputFormatter): public OutputFormatter {
+class VERTEX_CLASS_NAME(OutputFormatter) : public OutputFormatter {
 public:
-    void writeResult() {
-        int64_t vid;
-        double value;
-        char s[1024];
+	void writeResult() {
+		int64_t vid;
+		double value;
+		char s[1024];
 
-        for (ResultIterator r_iter; ! r_iter.done(); r_iter.next() ) {
-            r_iter.getIdValue(vid, &value);
-            int n = sprintf(s, "%lld: %f\n", (unsigned long long)vid, value);
-            writeNextResLine(s, n);
-        }
-    }
+		for (ResultIterator r_iter; !r_iter.done(); r_iter.next()) {
+			r_iter.getIdValue(vid, &value);
+			int n = sprintf(s, "%lld: %f\n", (unsigned long long)vid, value);
+			writeNextResLine(s, n);
+		}
+	}
 };
 
 // An aggregator that records a double value tom compute sum
-class VERTEX_CLASS_NAME(Aggregator): public Aggregator<int64_t> {
+class VERTEX_CLASS_NAME(Aggregator) : public Aggregator<int64_t> {
 public:
-    void init() {
+	void init() {
 		m_global = 0;
-
 		m_local = 0;
-    }
-    void* getGlobal() {
-        return &m_global;
-    }
-    void setGlobal(const void* p) {
-        m_global = * (int64_t *)p;
-    }
-    void* getLocal() {
-        return &m_local;
-    }
-    void merge(const void* p) {
+	}
+	void* getGlobal() {
+		return &m_global;
+	}
+	void setGlobal(const void* p) {
+		m_global = *(int64_t *)p;
+	}
+	void* getLocal() {
+		return &m_local;
+	}
+	void merge(const void* p) {
 		m_global += *(int64_t *)p;
-    }
-	// TODO
-    void accumulate(const void* p) {
+	}
+	void accumulate(const void* p) {
 		m_local += *(int64_t *)p;
-    }
+	}
 };
-// 顶点值Counter
-// 边值为double
-// 消息值为MyMsg
-class VERTEX_CLASS_NAME(): public Vertex <Counter, double, MyMsg> {
+
+class VERTEX_CLASS_NAME() : public Vertex <Counter, double, MyMsg> {
 public:
 	// TODO
 	// in A自身都可以检测出来
@@ -278,56 +273,55 @@ public:
 	}
 };
 
-class VERTEX_CLASS_NAME(Graph): public Graph {
+class VERTEX_CLASS_NAME(Graph) : public Graph {
 public:
-    VERTEX_CLASS_NAME(Aggregator)* aggregator;
+	VERTEX_CLASS_NAME(Aggregator)* aggregator;
 
 public:
-    // argv[0]: PageRankVertex.so
-    // argv[1]: <input path>
-    // argv[2]: <output path>
-    void init(int argc, char* argv[]) {
+	// argv[0]: PageRankVertex.so
+	// argv[1]: <input path>
+	// argv[2]: <output path>
+	void init(int argc, char* argv[]) {
 
-        setNumHosts(5);
-        setHost(0, "localhost", 1411);
-        setHost(1, "localhost", 1421);
-        setHost(2, "localhost", 1431);
-        setHost(3, "localhost", 1441);
-        setHost(4, "localhost", 1451);
+		setNumHosts(5);
+		setHost(0, "localhost", 1411);
+		setHost(1, "localhost", 1421);
+		setHost(2, "localhost", 1431);
+		setHost(3, "localhost", 1441);
+		setHost(4, "localhost", 1451);
 
-        if (argc < 3) {
-           printf ("Usage: %s <input path> <output path>\n", argv[0]);
-           exit(1);
-        }
+		if (argc < 3) {
+			printf("Usage: %s <input path> <output path>\n", argv[0]);
+			exit(1);
+		}
 
-        m_pin_path = argv[1];
-        m_pout_path = argv[2];
+		m_pin_path = argv[1];
+		m_pout_path = argv[2];
 
-        aggregator = new VERTEX_CLASS_NAME(Aggregator)[1];
-        regNumAggr(1);
-        regAggr(0, &aggregator[0]);
-    }
+		aggregator = new VERTEX_CLASS_NAME(Aggregator)[1];
+		regNumAggr(1);
+		regAggr(0, &aggregator[0]);
+	}
 
-    void term() {
-        delete[] aggregator;
-    }
+	void term() {
+		delete[] aggregator;
+	}
 };
-
 
 /* STOP: do not change the code below. */
 extern "C" Graph* create_graph() {
-    Graph* pgraph = new VERTEX_CLASS_NAME(Graph);
+	Graph* pgraph = new VERTEX_CLASS_NAME(Graph);
 
-    pgraph->m_pin_formatter = new VERTEX_CLASS_NAME(InputFormatter);
-    pgraph->m_pout_formatter = new VERTEX_CLASS_NAME(OutputFormatter);
-    pgraph->m_pver_base = new VERTEX_CLASS_NAME();
+	pgraph->m_pin_formatter = new VERTEX_CLASS_NAME(InputFormatter);
+	pgraph->m_pout_formatter = new VERTEX_CLASS_NAME(OutputFormatter);
+	pgraph->m_pver_base = new VERTEX_CLASS_NAME();
 
-    return pgraph;
+	return pgraph;
 }
 
 extern "C" void destroy_graph(Graph* pobject) {
-    delete ( VERTEX_CLASS_NAME()* )(pobject->m_pver_base);
-    delete ( VERTEX_CLASS_NAME(OutputFormatter)* )(pobject->m_pout_formatter);
-    delete ( VERTEX_CLASS_NAME(InputFormatter)* )(pobject->m_pin_formatter);
-    delete ( VERTEX_CLASS_NAME(Graph)* )pobject;
+	delete (VERTEX_CLASS_NAME()*)(pobject->m_pver_base);
+	delete (VERTEX_CLASS_NAME(OutputFormatter)*)(pobject->m_pout_formatter);
+	delete (VERTEX_CLASS_NAME(InputFormatter)*)(pobject->m_pin_formatter);
+	delete (VERTEX_CLASS_NAME(Graph)*)pobject;
 }
